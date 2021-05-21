cee327f4187261f83186d52527560e650d46d135
Evan Wappel <ewappel@gmail.com>
2021-05-20 19:26:54 -0700

    sync

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.listen(8080,function(){
-console.log("server is running")
+const mockUserData=[
+	{name:'Mark'},
+	{name:'Jill'}
+]
+
+app.get('/', function (req, res) {
+	res.json({
+		success: true,
+		message: 'this would be the default route',
+		users: mockUserData
+	})
+});
+
+
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
 })
+
+app.listen(8080,function(){console.log('server is listening')})
